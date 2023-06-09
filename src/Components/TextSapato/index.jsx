ee14a68c87156ebc34d485a9c32b6be195680423
Alisson Jose <alissonjose_57@hotmail.com>
2023-06-09 00:24:32 -0300

    changes-ecommerce-text1

--- src/Components/TextSapato/index.jsx
+++ src/Components/TextSapato/index.jsx
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 // eslint-disable-next-line no-unused-vars
-import style from './styles.css'
+import styles from './styles.css'
 
 
 export default function TextSapato() {
@@ -11,6 +11,9 @@ export default function TextSapato() {
       <h1>Melhores Compras</h1>
       <span className="sub-text1">Uma diferença que você pode sentir em sua sola</span>
       <span className="sub-text2">Nossa variedade de calçados, torna a experiência de nossos clientes cada vez melhor. A nossa linha de esportes, corridas e treinos foram preparados exclusivamente para um toque perfeito. </span>
+      <span className="sub-text3">FS 07 Sports Unisexx</span>
+      <span className="sub-text4">R$ 359,90</span>
+      <span className="sub-text5">por R$ 300,00 reais</span>
     </div>
 
   )
