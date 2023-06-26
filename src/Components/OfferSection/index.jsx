a1e8049f68585425a66a8e0c3905686430bb8778
Alisson Jose <alissonjose_57@hotmail.com>
2023-06-26 12:28:29 -0300

    changes-ecommerce-27

--- src/Components/OfferSection/index.jsx
+++ src/Components/OfferSection/index.jsx
@@ -2,15 +2,25 @@
 import React from 'react'
 import styles from "./styles.css"
 
+
 function Section2() {
     return (
         <div className="main-section">
             <div className='text'>
                 <p className='title'>Promoção em destaque</p>
                 <p className='offer'>60% em PROMOÇÃO!</p>
-                <p className='description'>Aproveite nossos descontos progressivos direto da fábrica. Além de <span>frete grátis</span> nas compras <span>acima de R$600,00</span></p>
+                <p className='description'>Aproveite nossos descontos progressivos direto da fábrica. Além de 
+                <span>frete grátis</span> nas compras 
+                <span>acima de R$600,00</span>
+                </p>
                 <button className="BuyHere2">Compre Aqui</button>
             </div>
+
+            <div className="second-section">
+                
+            </div>
+            
+            
         </div>
     )
 }
