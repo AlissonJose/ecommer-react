ba03b89c460d1ebf8118a479c8b3766b7baad10b
Alisson Jose <alissonjose_57@hotmail.com>
2023-06-29 12:58:36 -0300

    changes-ecommerce-30

--- src/Components/PostSection/index.jsx
+++ src/Components/PostSection/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 // eslint-disable-next-line no-unused-vars
-import styles from "./styles.css?inline"
+import styles from './styles.css'
 
 
 
@@ -10,9 +10,10 @@ export default function PostSection() {
     return (
       <section className="container-shoes-card">
         <div className="text-shoes-card">
-            <p className='title'>Categorias</p>
-            <p className="subtitle">Escolha o melhor estilo que se adapta ao seu toque</p>
-            <p className="description">Nossa variedade de estilos de sapatos se junta ao conforto e adaptabilidade do seus pés. Cores, texturas, formas, encontre a forma de se expressar no calçado.</p>
+            <p className="titleSection">Postagens recentes</p>
+            <p className="subtitleSection">Confira nosso Blog</p>
+            <p className="descriptionSection">Além de dicas valiosas para o seu dia a dia, estamos sempre atualizando e trazendo novidades para você.</p>
+            <p className="subDescriptionSection">Confira!</p>
         
              
                              
